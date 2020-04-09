26ac0c30d28a73869c0bb3d9aa32da44e328d2f7
Dem <d3m1ank@gmail.com>
2020-04-09 15:41:50 -0700

    Add files via upload

--- rubiks-cube-solver.py
+++ rubiks-cube-solver.py
@@ -65,6 +65,46 @@ class rubiksCube:
                 else:
                     newTopPosition = input("Enter next position: ")
                 self.bottom[i][j] = newTopPosition
+        print("\nCurrently editing FRONT positions. Please input new positions from left to right, top to bottom...")
+        for i in range(self.size):
+            for j in range(self.size):
+                if i == 0 and j == 0:
+                    newTopPosition = input("Enter new top left position: ")
+                elif i == self.size - 1 and j == self.size - 1:
+                    newTopPosition = input("Enter final bottom right position: ")
+                else:
+                    newTopPosition = input("Enter next position: ")
+                self.front[i][j] = newTopPosition
+        print("\nCurrently editing LEFT positions. Please input new positions from left to right, top to bottom...")
+        for i in range(self.size):
+            for j in range(self.size):
+                if i == 0 and j == 0:
+                    newTopPosition = input("Enter new top left position: ")
+                elif i == self.size - 1 and j == self.size - 1:
+                    newTopPosition = input("Enter final bottom right position: ")
+                else:
+                    newTopPosition = input("Enter next position: ")
+                self.left[i][j] = newTopPosition
+        print("\nCurrently editing BACK positions. Please input new positions from left to right, top to bottom...")
+        for i in range(self.size):
+            for j in range(self.size):
+                if i == 0 and j == 0:
+                    newTopPosition = input("Enter new top left position: ")
+                elif i == self.size - 1 and j == self.size - 1:
+                    newTopPosition = input("Enter final bottom right position: ")
+                else:
+                    newTopPosition = input("Enter next position: ")
+                self.back[i][j] = newTopPosition
+        print("\nCurrently editing RIGHT positions. Please input new positions from left to right, top to bottom...")
+        for i in range(self.size):
+            for j in range(self.size):
+                if i == 0 and j == 0:
+                    newTopPosition = input("Enter new top left position: ")
+                elif i == self.size - 1 and j == self.size - 1:
+                    newTopPosition = input("Enter final bottom right position: ")
+                else:
+                    newTopPosition = input("Enter next position: ")
+                self.right[i][j] = newTopPosition
 
     def isSolved(self):
         for i in self.top:
@@ -87,6 +127,50 @@ class rubiksCube:
                 return False
         return True
 
+    def save(self, filepath):
+        with open(filepath, 'w') as f:
+            for i in range(self.size):
+                for j in range(self.size):
+                    f.writelines(self.top[i][j] + '\n')
+            for i in range(self.size):
+                for j in range(self.size):
+                    f.writelines(self.bottom[i][j] + '\n')
+            for i in range(self.size):
+                for j in range(self.size):
+                    f.writelines(self.front[i][j] + '\n')
+            for i in range(self.size):
+                for j in range(self.size):
+                    f.writelines(self.left[i][j] + '\n')
+            for i in range(self.size):
+                for j in range(self.size):
+                    f.writelines(self.back[i][j] + '\n')
+            for i in range(self.size):
+                for j in range(self.size):
+                    f.writelines(self.right[i][j] + '\n')
+            f.close
+
+    def open(self, filepath):
+        with open(filepath, 'r') as f:
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.top[i][j] = str(f.readline())[:-1]
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.bottom[i][j] = str(f.readline())[:-1]
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.front[i][j] = str(f.readline())[:-1]
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.left[i][j] = str(f.readline())[:-1]
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.back[i][j] = str(f.readline())[:-1]
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.right[i][j] = str(f.readline())[:-1]
+            f.close
+
     def __str__(self):
         printStr = ""
 
@@ -106,8 +190,7 @@ class rubiksCube:
 
         for i in range(self.size):
             for j in range(self.size - 1, -1, -1):
-                print(j)
-                printStr += str(self.bottom[abs(i - self.size + 1)][j]) + ' ' # need to change order eventually
+                printStr += str(self.bottom[abs(i - self.size + 1)][j]) + ' '
             for j in range(self.size):
                 printStr += str(self.left[i][j]) + ' '
             for j in range(self.size):
@@ -115,7 +198,7 @@ class rubiksCube:
             for j in range(self.size):
                 printStr += str(self.right[i][j]) + ' '
             for j in range(self.size - 1, -1, -1):
-                printStr += str(self.bottom[abs(i - self.size + 1)][j]) + ' ' # need to change order eventually
+                printStr += str(self.bottom[abs(i - self.size + 1)][j]) + ' '
             printStr += '\n'
 
         for i in range(self.size):
@@ -134,9 +217,11 @@ class rubiksCube:
 
         return printStr
 
-xXx = rubiksCube(2)
+xXx = rubiksCube(3)
 print(xXx)
-print(xXx.isSolved())
-xXx.mixUpCube()
+# print(xXx.isSolved())
+# xXx.mixUpCube()
+# print(xXx)
+# print(xXx.isSolved())
+xXx.open("/Users/kelly/Desktop/myCube.txt")
 print(xXx)
-print(xXx.isSolved())
